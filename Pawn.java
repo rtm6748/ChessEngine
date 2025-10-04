5846967cef061b4e7724f049ab23932a7e286b5b
Rory Meyers <rtm6748@rit.edu>
2025-10-03 22:26:29 -0400

    pawn moves fully implemented.

--- Pawn.java
+++ Pawn.java
@@ -7,6 +7,30 @@ public class Pawn extends Piece{
 
     @Override
     public ArrayList<Square> getMoves(GameBoard currBoard, Square square) {
-        return null;
+        ArrayList<Square> validMoves = new ArrayList<>();
+        int forwardOrBackward = -1;
+        if (this.getColor() == Color.WHITE) {
+            forwardOrBackward = 1;
+        }
+        Square forward1 = new Square(this.getCurrSquare(), 0, forwardOrBackward);
+        if (isValid(forward1) && currBoard.getPiece(forward1).getColor() == Color.NONE) {
+            validMoves.add(forward1);
+            Square forward2 = new Square(this.getCurrSquare(), 0, 2 * forwardOrBackward);
+            boolean onStart = this.getCurrSquare().getY() == 1 && this.getColor() == Color.WHITE ||
+                              this.getCurrSquare().getY() == 7 && this.getColor() == Color.BLACK;
+            if (isValid(forward1) && currBoard.getPiece(forward2).getColor() == Color.NONE && onStart) {
+                validMoves.add(forward2);
+            }
+        }
+        Square left = new Square(this.getCurrSquare(), -1, forwardOrBackward);
+        Square right = new Square(this.getCurrSquare(), 1, forwardOrBackward);
+        Color other = this.getColor() == Color.WHITE ? Color.BLACK : Color.WHITE;
+        if (isValid(forward1) && currBoard.getPiece(left).getColor() == other) {
+            validMoves.add(left);
+        }
+        if (isValid(forward1) && currBoard.getPiece(right).getColor() == other) {
+            validMoves.add(right);
+        }
+        return validMoves;
     }
 }
